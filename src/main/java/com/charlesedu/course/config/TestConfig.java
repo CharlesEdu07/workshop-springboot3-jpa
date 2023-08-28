a294ad5947e45599b3a373ff1d815719aa9a7a32
unknown <charleseduardofaria@gmail.com>
2023-08-28 08:40:19 -0300

    Implementando status do pedido com enum

--- src/main/java/com/charlesedu/course/config/TestConfig.java
+++ src/main/java/com/charlesedu/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.charlesedu.course.entities.Order;
 import com.charlesedu.course.entities.User;
+import com.charlesedu.course.entities.enums.OrderStatus;
 import com.charlesedu.course.repositories.OrderRepository;
 import com.charlesedu.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Hally", "hally@gmail.com", "99999999", "12345");
         User user2 = new User(null, "John", "john@gmail.com", "88888888", "12345");
 
-        Order order1 = new Order(null, Instant.parse("2021-09-01T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2021-09-01T19:53:07Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2021-09-01T19:53:07Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2021-09-01T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2021-09-01T19:53:07Z"), OrderStatus.SHIPPED, user2);
+        Order order3 = new Order(null, Instant.parse("2021-09-01T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
 
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
