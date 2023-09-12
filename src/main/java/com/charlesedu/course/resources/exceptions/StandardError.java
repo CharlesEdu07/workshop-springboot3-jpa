a29035011bc99189b3cafaba53744255b9102329
unknown <charleseduardofaria@gmail.com>
2023-09-12 08:58:07 -0300

    Corrigindo classe StandardError

--- src/main/java/com/charlesedu/course/resources/exceptions/StandardError.java
+++ src/main/java/com/charlesedu/course/resources/exceptions/StandardError.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class StandartError implements Serializable {
+public class StandardError implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
@@ -16,10 +16,10 @@ public class StandartError implements Serializable {
     private String message;
     private String path;
 
-    public StandartError() {
+    public StandardError() {
     }
 
-    public StandartError(Instant timestamp, Integer status, String error, String message, String path) {
+    public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
         this.timestamp = timestamp;
         this.status = status;
         this.error = error;
